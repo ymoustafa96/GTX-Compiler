9f5009890b840d6f65a58c3a929a5e93115d61fd
ymoustafa96 <m3ypm@live.com>
2018-11-03 19:25:12 -0400

    Updated MyLexicalAnalyzer

--- src/main/scala/edu/towson/cosc/cosc455/ymoustafa/project1/LexicalAnalyzer.scala
+++ src/main/scala/edu/towson/cosc/cosc455/ymoustafa/project1/LexicalAnalyzer.scala
@@ -2,7 +2,7 @@ package edu.towson.cosc.cosc455.ymoustafa.project1
 
 trait LexicalAnalyzer {
   def addChar() : Unit
-  def getChar() : Char
-  def getNextToken() : String
+  def getChar() : Unit
+  def getNextToken() : Unit
   def lookup() : Boolean
 }
--- src/main/scala/edu/towson/cosc/cosc455/ymoustafa/project1/MyLexicalAnalyzer.scala
+++ src/main/scala/edu/towson/cosc/cosc455/ymoustafa/project1/MyLexicalAnalyzer.scala
@@ -1,11 +1,134 @@
 package edu.towson.cosc.cosc455.ymoustafa.project1
 
-class MyLexicalAnalyzer extends LexicalAnalyzer {
-  def addChar(): Unit = ???
+class MyLexicalAnalyzer extends LexicalAnalyzer{
+  var nextChar : Char = ' '
+  var tokenString : String = ""
+  var fileLength : Int = 0
 
-  def getChar(): Char = ???
+  //Adds chars to token string one at a time
+  def addChar(): Unit = {
+    tokenString += nextChar
+  }
 
-  def getNextToken(): String = ???
+  def getChar(): Unit = {
+    if (MyCompiler.position < fileLength) {
+      MyCompiler.position += 1
+      nextChar = MyCompiler.fileContents.charAt(MyCompiler.position)
+    }
+  }
 
-  def lookup(): Boolean = ???
+  def getNextToken(): Unit = {
+    fileLength = MyCompiler.fileContents.length -1
+    tokenString = ""
+
+    getChar()
+
+    if (fileLength != MyCompiler.position) {
+      //do nothing
+    }
+    if (Tokens.SPECIALCHAR.contains(nextChar)) {
+      if (Tokens.BOLD.contains(nextChar)) {
+        addChar()
+        getChar()
+      }
+      else if (Tokens.LISTITEM.contains(nextChar)) {
+        addChar()
+      }
+      else if (Tokens.ADDRESSE.contains(nextChar)) {
+        addChar()
+        getChar()
+      }
+      else if (Tokens.SPECIALCHAR(3) == nextChar) {
+        addChar()
+        if (Tokens.SPECIALCHAR(3) == nextChar) {
+          addChar()
+          MyCompiler.currentToken = tokenString
+          MyCompiler.position += 1
+          return
+        }
+        if (Tokens.SPECIALCHAR(6) == nextChar) {
+          addChar()
+          MyCompiler.currentToken = tokenString
+          return
+        }
+        if (Tokens.BRACKETE.contains(nextChar)) {
+          addChar()
+        }
+        if (Tokens.DOCE == tokenString.toUpperCase) {
+          if (MyCompiler.position - fileLength != 0) {
+            MyCompiler.position -= 1
+            getNextToken()
+            println("LEXICAL ERROR: Cannot have any symbols after \\END")
+            System.exit(1)
+          }
+        }
+      }
+      else if (Tokens.HEADING.contains(nextChar)) {
+        addChar()
+      }
+      else if (Tokens.IMAGEB.charAt(0) == nextChar) {
+        addChar()
+        getChar()
+        if (Tokens.IMAGEB.charAt(1) == nextChar) {
+          addChar()
+          if (lookup()) {
+            if (tokenString.substring(tokenString.length - 1, tokenString.length) == "\n"
+              || tokenString.substring(tokenString.length - 1, tokenString.length) == "\r"
+              || tokenString.substring(tokenString.length - 1, tokenString.length) == "\t") {
+              MyCompiler.currentToken = tokenString.substring(0, tokenString.length - 1)
+              return
+            }
+            else {
+              MyCompiler.currentToken = tokenString
+              return
+            }
+          }
+        }
+        else {
+          println("LEXICAL ERROR: Illegal character after '!'; '" + nextChar + "'")
+          System.exit(1)
+        }
+      }
+      else if (Tokens.SPECIALCHAR.contains(nextChar)) {
+        addChar()
+      }
+      if (tokenString.length > 0 && lookup()) {
+        if (tokenString.substring(tokenString.length - 1, tokenString.length) == "\n"
+          || tokenString.substring(tokenString.length - 1, tokenString.length) == "\r"
+          || tokenString.substring(tokenString.length - 1, tokenString.length) == "\t") {
+          MyCompiler.currentToken = tokenString.substring(0, tokenString.length - 1)
+        }
+        else MyCompiler.currentToken = tokenString
+      }
+      else {
+        println("LEXICAL ERROR: Illegal token: '" + tokenString + "'")
+        System.exit(1)
+      }
+    }
+    else if (nextChar.isLetterOrDigit ||nextChar==':' || nextChar=='.' || nextChar==',') {
+      addChar()
+      if (nextChar.toString.equals(Tokens.ADDRESSE)
+        || nextChar.toString.equals(Tokens.BRACKETE)
+        || nextChar.toString.equals(Tokens.PARAE)
+        || nextChar.toString.equals(Tokens.EQSIGN)
+        || nextChar.equals('\\')) {
+        //Will decrement index so special characters aren't skipped
+        MyCompiler.position -= 1
+      }
+      MyCompiler.currentToken = tokenString
+    }
+  }
+
+  def lookup(): Boolean = {
+    var temp: String = ""
+    if (tokenString.substring(tokenString.length-1,tokenString.length) == "\n"
+      || tokenString.substring(tokenString.length-1,tokenString.length) == "\r"
+      || tokenString.substring(tokenString.length-1,tokenString.length) == "\t") {
+      temp = tokenString.toUpperCase.substring(0,tokenString.length-1)
+      return Tokens.KEYWORDS.contains(temp)
+    }
+    else {
+      return Tokens.KEYWORDS.contains(tokenString.toUpperCase)
+    }
+  }
 }
